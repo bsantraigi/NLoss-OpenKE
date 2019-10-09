9a1e742451f155640b0487635a73227d42159459
Bishal Santra <bsantraigi@gmail.com>
2019-10-10 03:19:06 +0530

    vanilla works

--- vanilla_train_distmult.py
+++ vanilla_train_distmult.py
@@ -2,19 +2,19 @@ import config_old as config
 from  models_old import *
 import json
 import os
-data = "WN18RR"
+data = "FB15K237"
 os.environ['CUDA_VISIBLE_DEVICES']='0'
 con = config.Config()
 con.set_use_gpu(True)
 con.set_in_path(f"./benchmarks/{data}/")
 con.set_work_threads(8)
 con.set_train_times(1000)
-con.set_nbatches(100)	
+con.set_nbatches(1000)	
 con.set_alpha(0.1)
 con.set_bern(0)
 con.set_dimension(100)
 con.set_margin(1.0)
-con.set_ent_neg_rate(1)
+con.set_ent_neg_rate(5)
 con.set_rel_neg_rate(0)
 con.set_opt_method("adagrad")
 con.set_save_steps(10)
