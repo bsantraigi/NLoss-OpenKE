299c03fbf9c978c1ab28dcfb85b85e25b1c9b281
Bishal <bsantraigi@gmail.com>
2019-10-09 18:34:19 +0530

    retraining transE and distMult from openke

--- test_transe.py
+++ test_transe.py
@@ -2,11 +2,11 @@ import config
 from models import *
 import json
 import os 
-os.environ['CUDA_VISIBLE_DEVICES']='6'
+os.environ['CUDA_VISIBLE_DEVICES']='0'
 con = config.Config()
-con.set_use_gpu(False)
+con.set_use_gpu(True)
 #Input training files from benchmarks/FB15K/ folder.
-con.set_in_path("./benchmarks/FB15K/")
+con.set_in_path("./benchmarks/FB15K237/")
 #True: Input test files from the same folder.
 con.set_result_dir("./result")
 con.set_test_link(True)
