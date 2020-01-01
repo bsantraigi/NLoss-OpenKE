475534bb56dcf084ad4509a79666324e452ccb62
bs <bsantraigi@gmail.com>
2020-01-01 23:28:04 +0530

    things are stable

--- graph-tool-rwr.py
+++ graph-tool-rwr.py
@@ -12,7 +12,7 @@ import random
 5. Increase number of batches for all estimates
 '''
 
-data = "DB100K"
+data = "FB15k-237"
 
 rel = open(f'./benchmarks/{data}/relations.dict', 'r')
 ent = open(f'./benchmarks/{data}/entities.dict', 'r')
@@ -43,8 +43,12 @@ def create_graph(dataFileHandle):
     edge_list = []
     for lines in dataFileHandle:   #(u,v) -> {rel1, rel2, ..}
         lis = lines.split()
-        relations[(int(ent_dict[lis[0]]), int(ent_dict[lis[2]]))].append(lis[1])
-        edge_list.append((int(ent_dict[lis[0]]), int(ent_dict[lis[2]])))
+        u, v = int(ent_dict[lis[0]]), int(ent_dict[lis[2]])
+        if u == v:
+            # SKIP SELF LOOP
+            continue
+        relations[(u, v)].append(lis[1])
+        edge_list.append((u, v))
 
     print("# Edges:", len(edge_list))
     g.add_edge_list(edge_list)
@@ -246,6 +250,44 @@ class RWISG(Sampler):
         return len(self.minib_v)
 
 
+class RWRISG(Sampler):
+    def __init__(self, g, **kwargs):
+        assert 'restart_prob' in kwargs
+        super(RWRISG, self).__init__(g)
+        self.restart_prob = kwargs['restart_prob']
+        # self.v0 = self.v
+
+    def pack(self):
+        vfilt = self.g.new_vertex_property('bool')
+        for v in self.minib_v:
+            vfilt[v] = True
+
+        # Induced Subgraph
+        mini_g = GraphView(self.g, vfilt=vfilt)
+        # print(f"Induced MiniG: ({mini_g.num_vertices()}, {mini_g.num_edges()})")
+        self.batch_triples.append(mini_g.num_edges())
+        return mini_g
+
+    def sample_nxt(self):
+        if random.random() < self.restart_prob and len(self.minib_v) > 0:
+            # Change parent to a previously
+            # discovered vertex.
+            self.v = random.choice(list(self.minib_v))
+
+        nxt = self.v
+        self.minib_v.add(nxt)
+        n_list = list(self.v.out_neighbors())
+        while nxt == self.v:
+            k = random.randint(0, len(n_list) - 1)
+            nxt = n_list[k]
+        self.minib_e.append(self.g.edge(self.v, nxt))
+
+        return nxt
+
+    def sample_size(self):
+        return len(self.minib_v)
+
+
 class SimplyRandom(Sampler):
     def __init__(self, g, **kwargs):
         assert 'minib_size' in kwargs
@@ -273,16 +315,17 @@ def ed_vs_bs(SamplerClass, ax, rng_start, rng_end, rng_step):
     bss = []
     eds = []
 
-    nbatches = 10
+    nbatches = 100
+    rp = 0.8
     for _bs in np.arange(rng_start, rng_end, rng_step):
         print("\nTesting batch size", _bs)
-        sampler = SamplerClass(train_g, minib_size=_bs, restart_prob=0.4)
+        sampler = SamplerClass(train_g, minib_size=_bs, restart_prob=rp)
         bs, ed, _hist = sampler.remember_the_name(_bs, nbatches)
         print(bs, ed)
 
         bss.append(bs)
         eds.append(ed)
-
+    print(f"Restart Prob was {rp}.")
     ax.plot(bss, eds, marker='x')
     ax.set(xlabel="Batch Size (Number of triples)", ylabel="E[D] of Minibatch Graph",
            title=f"Expected Degree of Minibatch Graphs ({data})")
@@ -303,17 +346,24 @@ if __name__=="__main__":
     fig, ax = plt.subplots()
 
     # ed_vs_bs(random_walk, ax, 30, 10000, 1000)
+    bs_max = 8000
+    step = 1000
     print("================ SIMPLY RANDOM ==================")
-    ed_vs_bs(SimplyRandom, ax, 30, 8000, 1000)
+    ed_vs_bs(SimplyRandom, ax, 30, bs_max, step)
     print("================ RW ==================")
-    ed_vs_bs(RW, ax, 30, 8000, 1000)
+    ed_vs_bs(RW, ax, 30, bs_max, step)
     print("================ RWR ==================")
-    ed_vs_bs(RWR, ax, 30, 8000, 1000)
+    ed_vs_bs(RWR, ax, 30, bs_max, step)
+
+    bs_max = 2500
+    step = 250
     print("================ RWISG ==================")
-    ed_vs_bs(RWISG, ax, 10, 2500, 250)
+    ed_vs_bs(RWISG, ax, 10, bs_max, step)
+    print("================ RWRISG ==================")
+    ed_vs_bs(RWRISG, ax, 10, bs_max, step)
 
     ax.axhline(ed_full, linestyle='--')
-    ax.legend(['SR', 'RW', 'RWR', 'RWISG', 'Full KG'])
+    ax.legend(['SR', 'RW', 'RWR', 'RWISG', 'RWRISG', 'Full KG'])
 
     plt.show()
 
